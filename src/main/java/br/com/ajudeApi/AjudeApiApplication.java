8f5709bcc107e5b4eea498d3058a957372a22234
Waldir Marques <waldir.marques@dcx.ufpb.br>
2021-08-14 20:52:29 -0300

    first commit

---
+++ src/main/java/br/com/ajudeApi/AjudeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ajudeApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjudeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjudeApiApplication.class, args);
+	}
+
+}
